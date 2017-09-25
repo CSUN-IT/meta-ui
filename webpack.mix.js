ad04bc1b4ef9ed216db97f3edf872163e472e27d
Brian Canedo <bcanedo4@gmail.com>
2017-09-25 10:55:44 -0700

    Set public path

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,10 @@
 const mix = require('laravel-mix');
 
 mix.js('src/js/meta-ui.js', 'dist/js')
-   .sass('src/sass/meta-ui.scss', 'dist/css');
+   .sass('src/sass/meta-ui.scss', 'dist/css')
+
+
+
+
+
+mix.setPublicPath('./');
