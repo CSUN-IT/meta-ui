9e59f18d19926e40dd5a46f53f9fc5f65cc75166
Rahil Hashim <Rahil323@gmail.com>
2017-10-02 11:12:21 -0700

    removed containers.scss

--- dist/js/meta-ui.js
+++ dist/js/meta-ui.js
@@ -82,7 +82,7 @@ console.log('META+LAB UI Kit');
 /* 2 */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \n@import \"container\";\n^\n      File to import not found or unreadable: /Users/Rahil323/Build/meta-ui/src/sass/_container.scss.\nParent style sheet: stdin\n      in /Users/Rahil323/Build/meta-ui/src/sass/meta-ui.scss (line 4, column 1)\n    at runLoaders (/Users/Rahil323/Build/meta-ui/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /Users/Rahil323/Build/meta-ui/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /Users/Rahil323/Build/meta-ui/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/Users/Rahil323/Build/meta-ui/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/Users/Rahil323/Build/meta-ui/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.<anonymous> (/Users/Rahil323/Build/meta-ui/node_modules/async/dist/async.js:2244:31)\n    at Object.callback (/Users/Rahil323/Build/meta-ui/node_modules/async/dist/async.js:906:16)\n    at options.error (/Users/Rahil323/Build/meta-ui/node_modules/node-sass/lib/index.js:294:32)");
 
 /***/ })
 /******/ ]);
