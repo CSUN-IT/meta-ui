c4b34eb1f4dac2ec32d749f9955f114bcb0cb483
Brian Canedo <bcanedo4@gmail.com>
2017-10-11 11:45:49 -0700

    Multiple Image Automatic Carousel is finished, fixed a grid offset bug where any size col-4-offsets were set improperly.

--- dist/js/meta-ui.js
+++ dist/js/meta-ui.js
@@ -78,6 +78,34 @@ module.exports = __webpack_require__(2);
 // LET'S GET STARTED
 console.log('META+LAB UI Kit');
 
+// Maps names of included libraries to their filepaths.
+var includedDependencies = {};
+
+function includeOnce(file) {
+    var libraryName = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
+
+    if (libraryName === '') {
+        libraryName = file;
+    }
+    //If library is not included yet, then include it
+    if (!(libraryName in includedDependencies)) {
+        var script = document.createElement('script');
+        script.src = file;
+        script.type = 'text/javascript';
+        // script.defer = true;
+
+        document.head.appendChild(script);
+        // Add it our global list of libraries.
+        includedDependencies[libraryName] = file;
+    }
+}
+
+/* include any js files here */
+// includeOnce("https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js",'jquery');
+// includeOnce('../src/js/techCarousel.js');
+// includeOnce('../src/js/carousel.js');
+includeOnce('../src/js/multiCarousel.js');
+
 /***/ }),
 /* 2 */
 /***/ (function(module, exports) {
--- src/js/techCarousel.js
+++ src/js/techCarousel.js
@@ -8,7 +8,16 @@ function displaySlides() {
         slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
     slides[slideIndex-1].style.display = "block";
+    timer();
+}
+
+function timer() {
     setTimeout(displaySlides, 3000); // Change image every 3 seconds
 }
+
+// Add callback function
+// Make more functions
