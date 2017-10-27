02cf746146808305de70f98f791f0aec096cc4ef
Brian Canedo <bcanedo4@gmail.com>
2017-10-27 09:21:20 -0700

    Created a mixins folders and put meta-docs.scss in there. Modified webpack.mix.js to account for the directory change.

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,7 @@ const mix = require('laravel-mix');
 
 mix.js('src/js/meta-ui.js', 'dist/js')
    .sass('src/sass/meta-ui.scss', 'dist/css')
-    .sass('src/sass/meta-docs.scss', 'dist/css');
+    .sass('src/sass/mixins/meta-docs.scss', 'dist/css');
 
 
 // Needed for Windows Users for yarn run dev
