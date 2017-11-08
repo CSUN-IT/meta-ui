5caa97e8bdb3da0cfb6bc8362e02b3288503d954
Brian Canedo <bcanedo4@gmail.com>
2017-11-08 13:31:01 -0800

    Created Navbar

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,19 +1,55 @@
-    // $('.navbar__slide').on('change', function(){
-    //     $(this).toggle('.nav--slider');
-    // });
-// $(document).ready(function(){
-//     $(".navbar-select2").click(function(){
-//         $(".nav--slider").animate({right:'600px'});
-//     })
-// });
+$(document).ready(function() {
+    var navbar = $("#navbar");
+    var sticky = $(navbar).offset().top;
 
-$('.navbar-select2').on('click',function(event){
-    $('.nav--slider').show({right:'1000px'});
+    $(document).on('scroll', function() {
+        makeNavbarSticky();
+    });
+    function makeNavbarSticky() {
+        console.log(sticky);
+        if (window.pageYOffset >= sticky) {
+            $(navbar).addClass("sticky");
+            console.log(window.pageYOffset);
+        }
+        else {
+            $(navbar).removeClass("sticky");
+        }
+    }
+
+    $(".nav__icon--right").click(function() {
+        openNav();
+    });
+
+    function openNav() {
+        $("#mySlidenav").width('100%');
+        console.log("open clicked");
+    }
+
+    $(".closebtn").click(function() {
+        closeNav();
+    });
+
+    function closeNav() {
+        $("#mySlidenav").width(0);
+        console.log("close clicked");
+    }
+
+
+
+
+
+    $('.navbar-select2').on('click',function(event){
+        $('.nav--slider').show({right:'1000px'});
+    });
+
+    $(".nav--slider").hide();
+    $('.nav--slider').on('click', function(event){
+        event.preventDefault();
+        event.stopPropagation();
+        $(this).toggle('.closebtn');
+    });
+
 });
 
-$(".nav--slider").hide();
-$('.nav--slider').on('click', function(event){
-    event.preventDefault();
-    event.stopPropagation();
-    $(this).toggle('.closebtn');
-});
+
+
