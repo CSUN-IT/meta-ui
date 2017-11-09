3855699ad345e10510b3a3fb1ee8b012fc7df37b
Brian Canedo <bcanedo4@gmail.com>
2017-11-09 12:02:56 -0800

    Finished Navbar. Deleted (and stored in a different codepen) Josh's js that does not work with my code.

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,56 +1,25 @@
 $(document).ready(function() {
     var navbar = $("#navbar");
-    var sticky = $(navbar).offset().top;
-    var navTop = $(".nav--top");
 
-    $(document).on('scroll', function() {
-        makeNavbarSticky();
-    });
-    function makeNavbarSticky() {
-        console.log(sticky);
-        if (window.pageYOffset >= sticky) {
-            $(navbar).addClass("nav--sticky");
-            console.log(window.pageYOffset);
-        }
-        else {
-            $(navbar).removeClass("nav--sticky");
-        }
-    }
-
-    navTop.removeClass("nav--sticky");
-
+    // Click on btn triggers openNav()
     $(".nav__btn").click(function() {
         openNav();
     });
 
+    // JQuery to open navbar
     function openNav() {
         $("#mySlidenav").width('100%');
-        console.log("open clicked");
     }
 
+    // Click on btn triggers closeNav()
     $(".btn__close").click(function() {
         closeNav();
     });
 
+    // Closes the navbar
     function closeNav() {
         $("#mySlidenav").width(0);
-        console.log("close clicked");
     }
-
-
-
-
-
-    $('.navbar-select2').on('click',function(event){
-        $('.nav--slider').show({right:'1000px'});
-    });
-
-    $(".nav--slider").hide();
-    $('.nav--slider').on('click', function(event){
-        event.preventDefault();
-        event.stopPropagation();
-        $(this).toggle('.closebtn');
-    });
 
 });
 
