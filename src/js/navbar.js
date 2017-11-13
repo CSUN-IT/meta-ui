36fd8d46cc8567ec05188e6d90f1874187c23d81
Brian Canedo <bcanedo4@gmail.com>
2017-11-13 14:52:45 -0800

    Adding finishing touches, currently polishing nav button fade transition

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,14 +1,9 @@
 $(document).ready(function() {
     $('.nav--slider').hide();
-
-    $('.nav__btn').on('click', function(event){
+    $('.nav__btn').on('click', function(){
         $('#navbar').css('z-index', '3');
-        console.log(this);
         $('.nav--slider').toggle('.nav__btn', function(){
                 $('.nav__btn').toggleClass('fa-bars fa-times');
-                console.log(this);
-
-
         });
     });
 
